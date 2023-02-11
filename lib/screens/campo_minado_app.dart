94adc4693f98a26a2e2701c648f1b382d9b97138
Estelson Medeiros Pereira <estelson@gmail.com>
2023-02-11 07:03:04 -0300

    Configuração dos campos do tabuleiro para exibir as imagens de acordo com os números de bombas e tipos marcação

--- lib/screens/campo_minado_app.dart
+++ lib/screens/campo_minado_app.dart
@@ -1,4 +1,7 @@
+import 'package:campo_minado/components/campo_widget.dart';
 import 'package:campo_minado/components/resultado_widget.dart';
+import 'package:campo_minado/models/campo/campo.dart';
+import 'package:campo_minado/models/exceptions/explosao_exception.dart';
 import 'package:flutter/material.dart';
 
 class CampoMinadoApp extends StatelessWidget {
@@ -6,15 +9,40 @@ class CampoMinadoApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    Campo vizinho1 = Campo(linha: 1, coluna: 0);
+    vizinho1.minar();
+
+    Campo vizinho2 = Campo(linha: 1, coluna: 1);
+    vizinho2.minar();
+
+    Campo campo = Campo(linha: 0, coluna: 0);
+    campo.adicionarVizinho(vizinho1);
+    campo.adicionarVizinho(vizinho2);
+
+    try {
+      // campo.minar();
+      // campo.abrir();
+      campo.alternarMarcacao();
+    } on ExplosaoException {}
+
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         appBar: ResultadoWidget(
           venceu: false,
           onReiniciar: _reiniciar,
         ),
-        body: Text("Tabuleiro"),
+        body: CampoWidget(campo: campo, onAbrir: _abrir, onAlternarMarcacao: _alternarMarcacao),
       ),
     );
+  }
+
+  _abrir(Campo campo) {
+    debugPrint("abrir...");
+  }
+
+  _alternarMarcacao(Campo campo) {
+    debugPrint("alternar marcacao...");
   }
 
   _reiniciar() {
