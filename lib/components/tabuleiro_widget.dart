c4d993e55f67c55b2782eb3c2fff1865e887c1df
Estelson Medeiros Pereira <estelson@gmail.com>
2023-02-11 07:30:19 -0300

    Configuração do tabuleiro para exibir as os quadros na tela

---
+++ lib/components/tabuleiro_widget.dart
@@ -0,0 +1,28 @@
+import 'package:campo_minado/components/campo_widget.dart';
+import 'package:campo_minado/models/campo/campo.dart';
+import 'package:campo_minado/models/tabuleiro/tabuleiro.dart';
+import 'package:flutter/material.dart';
+
+class TabuleiroWidget extends StatelessWidget {
+  final Tabuleiro tabuleiro;
+  final void Function(Campo) onAbrir;
+  final void Function(Campo) onAlternarMarcacao;
+
+  const TabuleiroWidget({Key? key, required this.tabuleiro, required this.onAbrir, required this.onAlternarMarcacao}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: GridView.count(
+        crossAxisCount: tabuleiro.colunas,
+        children: tabuleiro.campos.map((campo) {
+          return CampoWidget(
+            campo: campo,
+            onAbrir: onAbrir,
+            onAlternarMarcacao: onAlternarMarcacao,
+          );
+        }).toList(),
+      ),
+    );
+  }
+}
--- lib/screens/campo_minado_app.dart
+++ lib/screens/campo_minado_app.dart
@@ -1,7 +1,7 @@
-import 'package:campo_minado/components/campo_widget.dart';
 import 'package:campo_minado/components/resultado_widget.dart';
+import 'package:campo_minado/components/tabuleiro_widget.dart';
 import 'package:campo_minado/models/campo/campo.dart';
-import 'package:campo_minado/models/exceptions/explosao_exception.dart';
+import 'package:campo_minado/models/tabuleiro/tabuleiro.dart';
 import 'package:flutter/material.dart';
 
 class CampoMinadoApp extends StatelessWidget {
@@ -9,22 +9,6 @@ class CampoMinadoApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    Campo vizinho1 = Campo(linha: 1, coluna: 0);
-    vizinho1.minar();
-
-    Campo vizinho2 = Campo(linha: 1, coluna: 1);
-    vizinho2.minar();
-
-    Campo campo = Campo(linha: 0, coluna: 0);
-    campo.adicionarVizinho(vizinho1);
-    campo.adicionarVizinho(vizinho2);
-
-    try {
-      // campo.minar();
-      // campo.abrir();
-      campo.alternarMarcacao();
-    } on ExplosaoException {}
-
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
@@ -32,7 +16,15 @@ class CampoMinadoApp extends StatelessWidget {
           venceu: false,
           onReiniciar: _reiniciar,
         ),
-        body: CampoWidget(campo: campo, onAbrir: _abrir, onAlternarMarcacao: _alternarMarcacao),
+        body: TabuleiroWidget(
+          tabuleiro: Tabuleiro(
+            linhas: 15,
+            colunas: 15,
+            qtdeBombas: 10,
+          ),
+          onAbrir: _abrir,
+          onAlternarMarcacao: _alternarMarcacao,
+        ),
       ),
     );
   }
